c17d98f52daff3474d2af58befc480c885b8cd3a
TRMichelson <123107239+TRMichelson@users.noreply.github.com>
2023-03-24 09:37:18 -0400

    Add files via upload

---
+++ .devcontainer/install_packages.R
@@ -0,0 +1,9 @@
+install.packages("duckdb")
+install.packages("pROC")
+install.packages("xgboost")
+install.packages("kernlab")
+install.packages("optparse")
+install.packages("devtools")
+
+require(devtools)
+install.packages("gbm", version='2.1.8.1')
