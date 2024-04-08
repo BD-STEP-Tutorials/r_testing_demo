60b16f20e8b9a17d4bfa61a2283d1c81ba2cd6c7
TRMichelson <123107239+TRMichelson@users.noreply.github.com>
2024-04-08 19:02:02 +0000

    Modified filepaths

--- src/starter_code.R
+++ src/starter_code.R
@@ -1,15 +1,11 @@
-## create a new package
+## create a new package in the root folder
 
-usethis::create_package("testingPackage")
+usethis::create_package("./r_documentation_demo/documentPackage")
 
 ## fill in Description and Add license
 
-usethis::use_mit_license()
+usethis::use_mit_license("./r_documentation_demo/documentPackage/")
 
 ## create a ReadMe file and skeleton
 
-usethis::use_readme_md()
-
-## create function scripts, placing them in the R script folder 'R/'
-usethis::use_r('beta_dist')
-usethis::use_r('gamma_dist')
+usethis::use_readme_md("./r_documentation_demo/documentPackage/")
