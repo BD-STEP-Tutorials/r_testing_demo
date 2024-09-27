0d00afec988f724228bad98b71d6ee6667e8fa2b
TRMichelson <123107239+TRMichelson@users.noreply.github.com>
2024-09-27 17:51:58 +0000

    adding code for testing to starter_code

--- src/starter_code.R
+++ src/starter_code.R
@@ -9,3 +9,28 @@ usethis::use_mit_license("./r_documentation_demo/documentPackage/")
 ## create a ReadMe file and skeleton
 
 usethis::use_readme_md("./r_documentation_demo/documentPackage/")
+
+## 
+usethis::use_testthat()
+
+## Creates "tests" folder
+## adds testthat as a suggests into your DESCRIPTION file
+## adds Config/testhat/edition into your DESCRIPTION file
+
+## Create a new test file based on the current file you have open in RStudio
+usethis::use_test()
+
+## Create a new test file with specified name
+uesthis::use_test("temporary")
+
+## Confirm your tests run
+devtools::test()
+
+## Check Test Coverage - % of lines of code in your packaged tested
+devtools::test_coverage()
+
+##
+
+
+
+
